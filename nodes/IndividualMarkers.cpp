ac7f941f9e31ce13648634c5ab6b8042425076f6
Bert Willaert <bert.willaert@intermodalics.eu>
2014-03-07 15:32:56 +0100

    nodes/IndMarkers: remove the cout with the detected marker IDs

--- nodes/IndividualMarkers.cpp
+++ nodes/IndividualMarkers.cpp
@@ -281,13 +281,23 @@ void GetMarkerPoses(IplImage *image, ARCloud &cloud) {
   if (marker_detector.Detect(image, cam, true, false, max_new_marker_error,
 			     max_track_error, CVSEQ, true)) 
     {
-      printf("\n--------------------------\n\n");
+
+/*	  std::string log = "Visible markers: ";
+      for (size_t i=0; i<marker_detector.markers->size(); i++){
+    	  Marker *m = &((*marker_detector.markers)[i]);
+    	  log.append("-");
+		  std::string id = static_cast<std::ostringstream*>( &(std::ostringstream() << (m->GetId())) )->str();
+          log.append(id);
+      }
+      ROS_INFO(log.c_str());
+*/
+	  //printf("\n--------------------------\n\n");
       for (size_t i=0; i<marker_detector.markers->size(); i++)
      	{
 	  vector<cv::Point> pixels;
 	  Marker *m = &((*marker_detector.markers)[i]);
 	  int id = m->GetId();
-	  cout << "******* ID: " << id << endl;
+	  //cout << "******* ID: " << id << endl;
 
 	  int resol = m->GetRes();
 	  int ori = m->ros_orientation;
